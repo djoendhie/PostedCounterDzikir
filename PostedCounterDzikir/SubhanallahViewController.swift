3b71326cb20b23a97c18c8cae70329eef7cf8b7b
Djoendhie <32661621+djoendhie@users.noreply.github.com>
2017-10-24 10:30:49 +0700

    lagi

--- PostedCounterDzikir/SubhanallahViewController.swift
+++ PostedCounterDzikir/SubhanallahViewController.swift
@@ -12,27 +12,24 @@ class SubhanallahViewController: UIViewController {
     @IBOutlet weak var labelNumber: UILabel!
     
     @IBOutlet weak var labelStepper: UIStepper!
-    @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        
-        
+    
+    @IBAction func btnStepper(_ sender: UIStepper) {
+
         labelNumber.text = Int(sender.value).description
-        
+
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        
-        //mengatur stepper wraps nya true
         labelStepper.wraps = true
         labelStepper.autorepeat = true
         labelStepper.maximumValue = 33
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
 }
